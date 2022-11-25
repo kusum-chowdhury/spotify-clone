d3112489d61b261b31a3b247eb2dd039ea6b3618
kusum chowdhury <kusumchowdhury5832@gmail.com>
2022-11-26 00:07:50 +0530

    added styling to the login page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content:  ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "green": "#1DB954",
+        "dark": "#191414",
+       
+      }
+    },
   },
   plugins: [],
 }
